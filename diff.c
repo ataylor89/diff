7e43e149475746aca1ec28c48807118b575ee5fb
Andrew <rolyata47@gmail.com>
2021-12-27 17:51:05 -0600

    added -b, -c, --bytes, --characters options

--- diff.c
+++ diff.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BYTES 0
+#define CHARACTERS 1
 
 int main(int argc, char **argv) {
     for (int i = 0; i < argc; i++) {
-        printf("argv[%d]: %s\n", i, argv[i]);
-    }
+        char *arg = argv[i];
+        printf("argv[%d]: %s\n", i, arg);
 
+        if (strcmp(arg, "-b") == 0 || strcmp(arg, "--bytes") == 0) {
+            printf("Mode: %s\n", "Bytes");
+        }
+        else if (strcmp(arg, "-c") == 0 || strcmp(arg, "--characters") == 0) {
+            printf("Mode: %s\n", "Characters");
+        }
+    }
+   
     return 0;
 }
