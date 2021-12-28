e70c84506a93051f5b9d6c262e33530d6c00358c
Andrew <rolyata47@gmail.com>
2021-12-27 21:39:08 -0600

    the diff program now compares two files with colored word analysis

--- diff.c
+++ diff.c
@@ -20,6 +20,12 @@ int get_file_size(FILE *file) {
     return size;
 }
 
+char *read_file(FILE *file, int filesize) {
+    char *buf = malloc(sizeof(char) * filesize);
+    fread(buf, sizeof(char), filesize, file);
+    return buf;
+}
+
 void diff(char* file1, char* file2, int mode) {
     printf("Running diff on files %s and %s in mode %d\n", file1, file2, mode);
     FILE *f1 = fopen(file1, "r");
@@ -28,14 +34,70 @@ void diff(char* file1, char* file2, int mode) {
     int size2 = get_file_size(f2);
     printf("Size of file1: %d\n", size1);
     printf("Size of file2: %d\n", size2);
+    char *buf1 = read_file(f1, size1);
+    char *buf2 = read_file(f2, size2);
+    
+    int len = size1 > size2 ? size1 : size2;
+
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < 16 && i+j < size1; j++) {
+            unsigned char a = (unsigned char) buf1[i+j];
+            unsigned char b = (unsigned char) buf2[i+j];
+
+            char *fmt;
+            if (j % 2 == 0)
+                fmt = "\%02hhx";
+            else if (j == 15 || i+j == size1-1)
+                fmt = "\%02hhx    ";
+            else
+                fmt = "\%02hhx ";
+
+            if (a == b) {
+                printf(fmt, a);
+            }
+            else {
+                printf(ANSI_COLOR_RED);
+                printf(fmt, a);
+                printf(ANSI_COLOR_RESET);
+            }
+        }
+        for (int j = 0; j < 16 && i+j < size2; j++) {
+            unsigned char a = (unsigned char) buf1[i+j];
+            unsigned char b = (unsigned char) buf2[i+j];
+            
+            char *fmt;
+            if (j % 2 == 0)
+                fmt = "\%02hhx";
+            else if (j == 15 || i+j==size2-1)
+                fmt = "\%02hhx\n";
+            else
+                fmt = "\%02hhx ";
+
+            if (a == b) {
+                printf(fmt, b);
+            }
+            else {
+                printf(ANSI_COLOR_GREEN);
+                printf(fmt, b);
+                printf(ANSI_COLOR_RESET);
+            }
+
+        }
+        i += 16;
+    }
     fclose(f1);
     fclose(f2);
 }
 
 int main(int argc, char **argv) {
+    if (argc < 3) {
+        printf("Usage: %s <options> <file1> <file2>\n", argv[0]);
+        return 0;
+    }
     for (int i = 0; i < argc; i++) {   
         printf("argv[%d]: %s\n", i, argv[i]);
     }
+    printf("size of char: %lu\n", sizeof(char));
     char *file1, *file2;
     int mode = 0;
     for (int i = 0; i < argc; i++) {
@@ -57,6 +119,9 @@ int main(int argc, char **argv) {
             file2 = arg;
         }
     }
-    diff(file1, file2, mode);   
+    if (file1 != NULL && file2 != NULL)
+        diff(file1, file2, mode);   
+    else
+        printf("Error parsing file1 and file2\n");
     return 0;
 }
