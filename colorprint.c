1d4f0d818dff31475a81bf8fbf0e8eff4f7afca3
Andrew <rolyata47@gmail.com>
2022-01-01 17:12:28 -0600

    Committing code for colorprint.c

--- colorprint.c
+++ colorprint.c
@@ -9,13 +9,6 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-#define RED 0
-#define GREEN 1
-#define YELLOW 2
-#define BLUE 3
-#define MAGENTA 4
-#define CYAN 5
-
 void colorprint(char *str, char *color) {
     if (strcmp(color, "red") == 0) {
         printf(ANSI_COLOR_RED);
@@ -57,9 +50,6 @@ int main(int argc, char **argv) {
         printf("Usage: %s <string> <color>", argv[0]);
         return 0;
     }
-    /*for (int i = 0; i < argc; i++) {
-        printf("argv[%d]: %s\n", i, argv[i]);
-    }*/
     colorprint(argv[1], argv[2]);
     return 0;
 }
